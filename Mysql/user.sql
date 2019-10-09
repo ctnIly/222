5e05c2399941023b37ae4d0ef00112ad3c485f53
ctnIly <55947280+ctnIly@users.noreply.github.com>
2019-10-09 21:58:33 +0800

    Add files via upload

---
+++ Mysql/user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user`(`id`, `name`, `password`, `sex`) VALUES (1, 'zxx', '123', 'm');
+INSERT INTO `user`(`id`, `name`, `password`, `sex`) VALUES (2, 'ctn', '123', 'null');
+INSERT INTO `user`(`id`, `name`, `password`, `sex`) VALUES (3, 'ctni', '1234', 'w');
+INSERT INTO `user`(`id`, `name`, `password`, `sex`) VALUES (5, 'www', '123', 'w');
+INSERT INTO `user`(`id`, `name`, `password`, `sex`) VALUES (6, 'zxx', '123', '男');
